498fed1097db18a375d5802f30de4df2788c5ccf
Crisaoo <Cristianocosta2019@outlook.com>
2020-09-16 11:55:06 -0300

    POST records

---
+++ backend/src/main/java/com/devsuperior/dspesquisa/resources/RecordResource.java
@@ -0,0 +1,29 @@
+package com.devsuperior.dspesquisa.resources;
+
+import java.net.URI;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.devsuperior.dspesquisa.dto.RecordDTO;
+import com.devsuperior.dspesquisa.dto.RecordInsertDTO;
+import com.devsuperior.dspesquisa.services.RecordService;
+
+@RestController
+@RequestMapping(value = "/records")
+public class RecordResource {
+	@Autowired
+	private RecordService service;
+	
+	@PostMapping
+	public ResponseEntity<RecordDTO> insert(@RequestBody RecordInsertDTO obj){
+		RecordDTO objDTO = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(objDTO.getId()).toUri();
+		return ResponseEntity.created(uri).body(objDTO);
+	}
+}
