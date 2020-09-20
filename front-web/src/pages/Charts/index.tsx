bc69473f3dd0afbe996f7db9ef3b12f178dbb2d4
Crisaoo <Cristianocosta2019@outlook.com>
2020-09-20 20:36:53 -0300

    Configuring front web for production

--- front-web/src/pages/Charts/index.tsx
+++ front-web/src/pages/Charts/index.tsx
@@ -21,7 +21,7 @@ const initialPieData = {
     series: []
 }
 
-const BASE_URL = 'http://localhost:8080';
+const BASE_URL = 'https://sds1-crisao.herokuapp.com'
 
 const Charts = () => {
     const [barChartData, setBarCharData] = useState<BarChartData[]>([]);
